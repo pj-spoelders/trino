860a080c3ea76ef7dee9a5b595b3df84d9c6522b
Pieterjan Spoelders <pj.spoelders@gmail.com>
2026-01-09 07:45:50 -0500

    10 seconds test

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeExasol.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/env/environment/EnvMultinodeExasol.java
@@ -58,7 +58,7 @@ public class EnvMultinodeExasol
     {
         DockerContainer container = new DockerContainer("exadockerci4/docker-db:2025.1.8_dev_java_slc_only", "exasol") //Test container tailored to reduce used disk space and solve CI disk space pressure issue.
                 .withStartupCheckStrategy(new IsRunningStartupCheckStrategy())
-                .waitingFor(forSelectedPorts(EXASOL_PORT).withStartupTimeout(java.time.Duration.ofMinutes(10)))
+                .waitingFor(forSelectedPorts(EXASOL_PORT).withStartupTimeout(java.time.Duration.ofSeconds(10)))
                 .withEnv("COSLWD_ENABLED", "1") //Disables rsyslogd, cleans up log clutter and speeds up database startup         
                 .withStartupAttempts(3); 
         container.setPrivilegedMode(true);
